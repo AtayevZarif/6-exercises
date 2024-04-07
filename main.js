c771057c480621d81f0d2154e411f58792695b9b
AtayevZarif <atayev1234567890@gmail.com>
2024-04-07 22:05:56 +0500

    Succsess

---
+++ main.js
@@ -0,0 +1,91 @@
+// 1
+
+let twoNumbers = (a,b) =>{
+    console.log(a);
+    console.log(b);
+}
+
+twoNumbers(3,5)
+
+
+// 2
+
+// let toNumber = (c) =>{
+//     let number = Number(c)
+//     alert(` ${c} ${typeof(c)} \n ${number} ${typeof(number)}`)
+// }
+
+// toNumber("12")
+
+
+// 3
+
+// let oddEven = (d) =>{
+//     alert(d)
+//     if (d % 2 == 0){
+//         alert(`${d} - juft son`)
+//     }
+//     else {
+//         alert(`${d} - toq son`)
+//     }
+// }
+
+// oddEven(Math.ceil(Math.random() * 100))
+
+
+// 4
+
+// let any = prompt("Ixtiyorit son yozing")
+
+// let mars = function(e){
+//     if (any.toLowerCase() == e.toLowerCase()){
+//         alert(any)
+//     }
+//     else {
+//         alert("Mars IT school")
+//     }
+// }
+
+// mars("Mars")
+
+
+// 5
+
+// let words = prompt("5ta so'z yozing")
+// let split = words.split(" ")
+// const fiveOrBigger = []
+// const smallerFive = []
+
+// let wordsFunction = function(f){
+//     for (const word of split) {
+//         if (word.length >= f){
+//             fiveOrBigger.push(word)
+//         }
+//         else {
+//             smallerFive.push(word)
+//         }
+//     }
+//     alert(fiveOrBigger + "\n" + smallerFive);
+// }
+
+// wordsFunction(5)
+
+
+// 6
+
+// let oddEven2 = function(g){
+//     const even = []
+//     const odd = []
+//     for (let i = 1; i <= g; i++) {
+//         if (i % 2 == 0){
+//             even.push(i)
+//         }
+//         else {
+//             odd.push(i)
+//         }
+//     }
+//     console.log(`Juft: ${even}`);
+//     console.log(`Toq: ${odd}`);
+// }
+
+// oddEven2(100)
